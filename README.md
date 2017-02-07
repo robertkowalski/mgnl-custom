9f7339b3888285b17b9c550604065db2523c25a7
Robert Kowalski <rok@kowalski.gd>
2017-02-07 14:10:52 +0000

    add integration tests
    adds a testmatrix for different magnolia versions to run
    integration tests based on bash or cheerio.
    
    uses a bootstrap.xml to setup the page.

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 # mgnl-custom
 
 Small demo module built with webpack.
+
+
+## Tests
+
+### Fixture Setup
+
+Fixture was exported from Magnolia using the "Export"-functionality of the UI
+
+### JS Unit Tests
+
+Unit tests are using Jest as test runner, as it supplies a DOM environment
+
+### Template / Integration tests
+
+Tests are super simple and based `cheerio`. Mocha is used as test runner
+
+### Travis
+
+Main test step in Travis executes `npm test`
+
+Setting up the Magnolia server is done with the `before_script` hook.
---
+++ dev/test/integration/cheerio.js
@@ -0,0 +1,38 @@
+// mocha based tests
+
+const assert = require('assert')
+const request = require('request')
+const cheerio = require('cheerio')
+
+const URL = 'http://localhost:8080/magnoliaPublic/ci-testpage.html'
+
+describe('integration test', () => {
+
+  it('renders the template with the css class furbie', (done) => {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0'
+      },
+      uri: URL
+    }, (err, res, body) => {
+      const $ = cheerio.load(body)
+      assert.equal($('.furbie').length, 1)
+      done()
+    })
+  }).timeout(10000)
+
+  it('renders the template variable in the script tag', (done) => {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0'
+      },
+      uri: URL
+    }, (err, res, body) => {
+      const $ = cheerio.load(body)
+      const scriptTagContent = $('script').text()
+      assert.ok(/\.furbie/.test(scriptTagContent), '.furbie in script')
+      done()
+    })
+  })
+
+})
