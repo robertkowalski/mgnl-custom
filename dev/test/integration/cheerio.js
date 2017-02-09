12961adf56cc334f9da72b5678eec1e61e7cc1d1
Robert Kowalski <rok@kowalski.gd>
2017-02-09 11:50:42 +0000

    add user agent

--- dev/test/integration/cheerio.js
+++ dev/test/integration/cheerio.js
@@ -9,7 +9,12 @@ const URL = 'http://localhost:8080/magnoliaPublic/ci-testpage.html'
 describe('integration test', () => {
 
   it('renders the template with the css class furbie', (done) => {
-    request(URL, (err, res, body) => {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0'
+      },
+      uri: URL
+    }, (err, res, body) => {
       const $ = cheerio.load(body)
       assert.equal($('.furbie').length, 1)
       done()
@@ -17,7 +22,12 @@ describe('integration test', () => {
   })
 
   it('renders the template variable in the script tag', (done) => {
-    request(URL, (err, res, body) => {
+    request({
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:51.0) Gecko/20100101 Firefox/51.0'
+      },
+      uri: URL
+    }, (err, res, body) => {
       const $ = cheerio.load(body)
       const scriptTagContent = $('script').text()
       assert.ok(/\.furbie/.test(scriptTagContent), '.furbie in script')
