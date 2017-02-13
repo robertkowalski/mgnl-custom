04507cb25b1b60f5c9597b627925c69b5d6362bb
Robert Kowalski <rok@kowalski.gd>
2017-02-13 15:27:28 +0000

    use css loader with less to inline styles in JS

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,4 @@
+import Style from '../css/calendar.less'
 import Calendar from './calendar.js'
 
 window.Calendar = Calendar
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'less-loader'
+        ]
       }
     ]
   }
