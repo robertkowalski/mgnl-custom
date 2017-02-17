59cb899f48d96489cf9b58bc40885a95b0029dc5
Robert Kowalski <rok@kowalski.gd>
2017-02-17 13:26:01 +0000

    rename dev to _dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './dev/js/app.js',
+  entry: './_dev/js/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'webresources', 'js')
